f841672224fe7b7783ee5bbd99ca99fe06df5693
sbpolicar <sbpolicar@gmail.com>
2016-01-20 10:46:32 -0800

    sdfkje

--- controllers/HomeCtrl.js
+++ controllers/HomeCtrl.js
@@ -1,7 +1,7 @@
-BakeryApp.controller('HomeCtrl', ['$scope', '$location', function($scope, $location){
-  // console.log('this is the HomeCTRL')
-  $scope.$on('$routeChangeStart', function() {
-    // $location.path() = '/'
-  });
+// BakeryApp.controller('HomeCtrl', ['$scope', '$location', function($scope, $location){
+//   // console.log('this is the HomeCTRL')
+//   $scope.$on('$routeChangeStart', function() {
+//     // $location.path() = '/'
+//   });
 
-}])
+// }])
