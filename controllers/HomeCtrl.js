651789b1b03ada4a9e1fd5d161323232c1554cff
sbpolicar <sbpolicar@gmail.com>
2016-01-20 10:14:46 -0800

    working so far

---
+++ controllers/HomeCtrl.js
@@ -0,0 +1,7 @@
+BakeryApp.controller('HomeCtrl', ['$scope', '$location', function($scope, $location){
+  // console.log('this is the HomeCTRL')
+  $scope.$on('$routeChangeStart', function() {
+    // $location.path() = '/'
+  });
+
+}])
---
+++ js/script.js
@@ -0,0 +1,41 @@
+var BakeryApp = angular.module('BakeryApp',['ngMaterial','ngAnimate','ngRoute']);
+
+BakeryApp.run([function(){
+  // console.log('Bakery angular init');
+}]);
+
+BakeryApp.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider) {
+
+  // $locationProvider.html5Mode(true);
+
+  $routeProvider
+  .when('/',{
+    templateUrl: '/views/home.html',
+    controller: 'HomeCtrl'
+  })
+  .when('/special',{
+    templateUrl: '/views/special.html',
+    controller: 'HomeCtrl'
+  })
+  .when('/about',{
+    templateUrl: '/views/about.html',
+    controller: 'HomeCtrl'
+  })
+  .when('/current',{
+    templateUrl: '/views/current.html',
+    controller: 'HomeCtrl'
+  })
+  .when('/history',{
+    templateUrl: '/views/history.html',
+    controller: 'HomeCtrl'
+  })
+  .when('/login',{
+    templateUrl: '/views/admin.html',
+    controller: 'HomeCtrl'
+  })
+  .otherwise({
+    templateUrl:'/views/404.html'
+  })
+
+}])
+
