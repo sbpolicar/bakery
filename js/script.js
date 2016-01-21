08d595a391db0e9845a231b52549c7adda4b2273
sbpolicar <sbpolicar@gmail.com>
2016-01-21 15:36:52 -0800

    changed should work now?

--- js/script.js
+++ js/script.js
@@ -10,27 +10,27 @@ BakeryApp.config(['$routeProvider','$locationProvider',function($routeProvider,$
   $locationProvider.html5Mode(false);
 
   $routeProvider
-  .when('/',{
+  .when('/bakery/#/',{
     templateUrl: '/views/home.html'
 
   })
-  .when('/menu',{
+  .when('/bakery/#/menu',{
     templateUrl: '/views/menu.html'
 
   })
-  .when('/about',{
+  .when('/bakery/#/about',{
     templateUrl: '/views/about.html'
 
   })
-  .when('/current',{
+  .when('/bakery/#/current',{
     templateUrl: '/views/current.html'
 
   })
-  .when('/history',{
+  .when('/bakery/#/history',{
     templateUrl: '/views/history.html'
 
   })
-  .when('/login',{
+  .when('/bakery/#/login',{
     templateUrl: '/views/admin.html'
 
   })
